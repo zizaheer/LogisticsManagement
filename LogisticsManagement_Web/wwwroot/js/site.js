85b6142538cb6ccf2eca302378223f9cf09cec85
Kazi Islam <zizaheer@hotmail.com>
2019-03-25 07:53:19 -0400

    ok

--- LogisticsManagement_Web/wwwroot/js/site.js
+++ LogisticsManagement_Web/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 // Write your JavaScript code.
-// Created on March 2019. Overall site js may come here
+
 // Please refer to bundleconfig.json where the js files are being minified as site.min.js which is referenced on _Layout template.
-// Every UI may have its own js file and can be bundled together
+// Every UI may have its own js file and recommended to bundle together in site.min.js
 
