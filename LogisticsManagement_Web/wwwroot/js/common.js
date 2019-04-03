5f412207be7427b96ce3e539feddeac883e291fa
Kazi Islam <zizaheer@hotmail.com>
2019-04-03 11:56:25 -0400

    ok

--- LogisticsManagement_Web/wwwroot/js/common.js
+++ LogisticsManagement_Web/wwwroot/js/common.js
@@ -1,1 +1,22 @@
 ﻿
+
+function GetListObject(controllerName, actionName) {
+
+    var actionUrl = controllerName + '/' + actionName;
+
+    $.ajax({
+        url: actionUrl,
+        type: 'GET',
+        dataType: 'json',
+        success: function (result) {
+            return result;
+        },
+        error: function (result) {
+            return result;
+        }
+    });
+
+
+
+
+}
