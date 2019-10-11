2e97f79243856f3fe6b55f7e64e1951636195412
kazi <kazi@instawize.com>
2019-10-11 18:28:53 -0400

    ok

--- LogisticsManagement_Web/Controllers/AdditionalServiceController.cs
+++ LogisticsManagement_Web/Controllers/AdditionalServiceController.cs
@@ -20,6 +20,7 @@ namespace LogisticsManagement_Web.Controllers
         private Lms_AdditionalServiceLogic _additionalServiceLogic;
         private readonly LogisticsContext _dbContext;
         IMemoryCache _cache;
+        SessionData sessionData = new SessionData();
 
         public AdditionalServiceController(IMemoryCache cache, LogisticsContext dbContext)
         {
@@ -30,8 +31,8 @@ namespace LogisticsManagement_Web.Controllers
 
         public IActionResult Index()
         {
-            var customerList = _additionalServiceLogic.GetList();
-            return View();
+            var serviceList = _additionalServiceLogic.GetList();
+            return View(serviceList);
         }
 
         public JsonResult GetAdditionalServiceList()
@@ -52,5 +53,117 @@ namespace LogisticsManagement_Web.Controllers
 
             return Json(string.Empty);
         }
+
+        [HttpGet]
+        public IActionResult PartialViewDataTable()
+        {
+            var serviceList = _additionalServiceLogic.GetList();
+            return PartialView("_PartialViewServiceData", serviceList);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add([FromBody]dynamic serviceData)
+        {
+            ValidateSession();
+            var result = "";
+
+            try
+            {
+                if (serviceData != null)
+                {
+                    Lms_AdditionalServicePoco servicePoco = JsonConvert.DeserializeObject<Lms_AdditionalServicePoco>(JsonConvert.SerializeObject(serviceData));
+
+                    if (servicePoco.Id < 1 && servicePoco.ServiceName.Trim() != string.Empty)
+                    {
+                        servicePoco.CreatedBy = sessionData.UserId;
+                        var address = _additionalServiceLogic.Add(servicePoco);
+
+                        result = "Success";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromBody]dynamic serviceData)
+        {
+            ValidateSession();
+            var result = "";
+
+            try
+            {
+                if (serviceData != null)
+                {
+                    Lms_AdditionalServicePoco addressPoco = JsonConvert.DeserializeObject<Lms_AdditionalServicePoco>(JsonConvert.SerializeObject(serviceData));
+
+                    if (addressPoco.Id > 0 && addressPoco.ServiceName.Trim() != string.Empty)
+                    {
+                        var existingService = _additionalServiceLogic.GetSingleById(addressPoco.Id);
+                        // it is required to pull existing data first, 
+                        // cause there are some data which do not come from UI
+
+                        existingService.ServiceName = addressPoco.ServiceName;
+                        existingService.ServiceCode = addressPoco.ServiceCode;
+                        existingService.PayToDriver = addressPoco.PayToDriver;
+                        existingService.IsTaxApplicable = addressPoco.IsTaxApplicable;
+                        existingService.IsApplicableForStorage = addressPoco.IsApplicableForStorage;
+                        existingService.IsActive = addressPoco.IsActive;
+
+                        var poco = _additionalServiceLogic.Update(existingService);
+                        result = poco.Id.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public IActionResult Remove(string id)
+        {
+            var result = "";
+            try
+            {
+                var poco = _additionalServiceLogic.GetSingleById(Convert.ToInt32(id));
+                if (poco != null)
+                {
+                    _additionalServiceLogic.Remove(poco);
+                    result = "Success";
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(result);
+        }
+
+        private void ValidateSession()
+        {
+            if (HttpContext.Session.GetString("SessionData") != null)
+            {
+                sessionData = JsonConvert.DeserializeObject<SessionData>(HttpContext.Session.GetString("SessionData"));
+                if (sessionData == null)
+                {
+                    Response.Redirect("Login/Index");
+                }
+            }
+            else
+            {
+                Response.Redirect("Login/InvalidLocation");
+            }
+        }
+
     }
 }
---
+++ LogisticsManagement_Web/wwwroot/js/additional-service.js
@@ -0,0 +1,138 @@
+﻿
+$(document).ready(function () {
+
+    $(document).ajaxStart(function () {
+        $("#spinnerLoadingDataTable").css("display", "inline-block");
+    });
+    $(document).ajaxComplete(function () {
+        $("#spinnerLoadingDataTable").css("display", "none");
+    });
+});
+
+
+$('#btnAddNewAddService').on('click', function () {
+    $('#txtServiceId').prop('readonly', true);
+    $('#txtServiceId').val('');
+    $('#frmAddServiceForm').trigger('reset');
+
+    $('#additionalService').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+
+    $('#additionalService').draggable();
+    $('#additionalService').modal('show');
+});
+
+
+$('#additional-service-list').on('click', '.btnEdit', function () {
+    $('#txtServiceId').prop('readonly', true);
+    var serviceInfo = null;
+    var serviceid = $(this).data('serviceid');
+    var serviceInfoRaw = GetSingleById('AdditionalService/GetAdditionalServiceInfoById', serviceid);
+
+    if (serviceInfoRaw !== "") {
+        serviceInfo = JSON.parse(serviceInfoRaw);
+    }
+    else {
+        bootbox.alert('The service was not found.');
+        event.preventDefault();
+        return;
+    }
+
+    FillServiceInfo(serviceInfo);
+
+    $('#additionalService').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+
+    $('#additionalService').draggable();
+    $('#additionalService').modal('show');
+});
+
+$('#btnCloseModal').on('click', function () {
+    $('#additionalService').modal('hide');
+});
+
+
+$('#frmAddServiceForm').on('keyup keypress', function (e) {
+    var keyCode = e.keyCode || e.which;
+    if (keyCode === 13) {
+        e.preventDefault();
+        return false;
+    }
+});
+$('#frmAddServiceForm').unbind('submit').submit(function (event) {
+    event.preventDefault();
+
+    var data = GetFormData();
+
+    if (data.serviceName ==='') {
+        bootbox.alert('Please enter service name');
+        return;
+    }
+
+    if (data.id > 0) {
+        PerformPostActionWithObject('AdditionalService/Update', data);
+    }
+    else {
+        var result = PerformPostActionWithObject('AdditionalService/Add', data);
+    }
+
+    location.reload();
+});
+
+$('.btnDelete').unbind().on('click', function () {
+    serviceId = $(this).data('serviceid');
+    PerformPostActionWithId('AdditionalService/Remove', serviceId);
+    location.reload();
+
+});
+
+function GetFormData() {
+    var data = {
+        id: $('#hfServiceId').val() === '' ? 0 : $('#hfServiceId').val(),
+        serviceCode: $('#txtShortCode').val(),
+        serviceName: $('#txtServiceName').val(),
+        isTaxApplicable: $('#chkIsTaxApplicable').is(':checked') === true ? 1 : 0,
+        payToDriver: $('#chkPayToDriver').is(':checked') === true ? 1 : 0,
+        isApplicableForStorage: $('#chkIsApplicableForStorage').is(':checked') === true ? 1 : 0,
+        isActive: $('#chkIsActive').is(':checked') === true ? 1 : 0
+    };
+
+    return data;
+}
+
+function FillServiceInfo(serviceInfo) {
+
+    $('#hfServiceId').val(serviceInfo.Id);
+    $('#txtServiceId').val(serviceInfo.Id);
+    $('#txtShortCode').val(serviceInfo.ServiceCode);
+    $('#txtServiceName').val(serviceInfo.ServiceName);
+
+    if (serviceInfo.IsTaxApplicable === true) {
+        $('#chkIsTaxApplicable').prop('checked', true);
+    } else {
+        $('#chkIsTaxApplicable').prop('checked', false);
+    }
+
+    if (serviceInfo.PayToDriver === true) {
+        $('#chkPayToDriver').prop('checked', true);
+    } else {
+        $('#chkPayToDriver').prop('checked', false);
+    }
+
+    if (serviceInfo.IsApplicableForStorage === true) {
+        $('#chkIsApplicableForStorage').prop('checked', true);
+    } else {
+        $('#chkIsApplicableForStorage').prop('checked', false);
+    }
+
+    if (serviceInfo.IsActive === true) {
+        $('#chkIsActive').prop('checked', true);
+    } else {
+        $('#chkIsActive').prop('checked', false);
+    }
+}
+
